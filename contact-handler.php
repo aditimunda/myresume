7261cad0dd86559e1df35ad07d121f83c55ae907
Aditi Munda <aditi.munda@gmail.com>
2021-12-09 15:29:56 +0530

    upload contact handler.php file

---
+++ contact-handler.php
@@ -0,0 +1,27 @@
+<?php
+
+	$first_name = $_POST['first_name']; 
+	$last_name = $_POST['last_name']; 
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message']; 
+
+	
+	$email_form = 'aditi.munda@gmail.com';
+	
+	$email_subject = "New Form Submission";
+
+	$email_body = "User First Name: $first_name. \n".
+						"Last Name: $last_name. \n".
+							"User Email: $visitor_email.\n"'
+								"User Message: $message.\n";
+
+	$to ="aditi.munda@gmail.com";
+	
+	$headers .="From: $email_form \r\n";
+
+	$headers .= "Reply-To: $visitor_email \r\n";
+
+	mail($to, $email_subject, $email_body, $headers);
+
+	header("Location: contact.html");
+?>
