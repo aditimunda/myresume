1b4a259df12799504a2366e0c0761c5c972c7e33
Aditi Munda <aditi.munda@gmail.com>
2021-12-15 16:40:11 +0530

    Update contact-handler.php

--- contact-handler.php
+++ contact-handler.php
@@ -1,27 +1,18 @@
 <?php
-
+if (isset($_POST['submit'])){
+	
 	$first_name = $_POST['first_name']; 
 	$last_name = $_POST['last_name']; 
-	$visitor_email = $_POST['email'];
+	$mailFrom = $_POST['mail'];
 	$message = $_POST['message']; 
 
 	
-	$email_from = 'aditi.munda@gmail.com';
+	$mailTo ="aditi.munda@outlook.com";
+	$headers .="From: ".$mailFrom;
+	$txt = "You have received an e-mail from ".$first_name.".\n\n".$message;
 	
-	$email_subject = "New Form Submission";
+	mail($mailTo, $message, $txt, $headers);
+	header("Location: contact.html?mailsend");
 
-	$email_body = "User First Name: $first_name. \n".
-						"Last Name: $last_name. \n".
-							"User Email: $visitor_email. \n".
-								"User Message: $message. \n";
-
-	$to ="aditi.munda@gmail.com";
-	
-	$headers .="From: $email_from \r\n";
-
-	$headers .= "Reply-To: $visitor_email \r\n";
-
-	mail($to, $email_subject, $email_body, $headers);
-
-	header("Location: contact.html");
+}	
 ?>
